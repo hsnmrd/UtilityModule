7ae4fac1e3e95aafe8a6be02f6c34b4d943a0a27
ASUS ROG <hsn.moradnezhad@gmail.com>
2021-03-21 15:43:30 +0330

    Utility Module

---
+++ app/src/main/java/com/raika/utilitymodule/ToastingModule.kt
@@ -0,0 +1,20 @@
+package com.raika.utilitymodule
+
+import android.app.Activity
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.textview.MaterialTextView
+
+fun Activity.toasting(message: String) {
+    val inflater = this.layoutInflater
+    val layout: ViewGroup = inflater.inflate(R.layout.utility_module_toasting, null) as ViewGroup
+    val text: MaterialTextView = layout.findViewById(R.id.tv_utility_module_toasting_root)
+    text.text = message
+    with (Toast(this)) {
+        setGravity(Gravity.CENTER_VERTICAL and Gravity.BOTTOM, 0, 0)
+        duration = Toast.LENGTH_LONG
+        view = layout
+        show()
+    }
+}
